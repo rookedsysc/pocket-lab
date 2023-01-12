0b08e6c80b8c64ed2860426616b6fcdfa0e7f7c4
rookedsysc <rookedsysc36@gmail.com>
2023-01-13 02:55:11 +0900

    feat : 한 달 통계 표시해주는 Header 생성(#6)

--- lib/calendar/view/calendar_screen.dart
+++ lib/calendar/view/calendar_screen.dart
@@ -1,14 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:pocket_lab/calendar/component/calendar.dart';
+import 'package:pocket_lab/calendar/component/month_header.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class CalendarScreen extends StatelessWidget {
-  const CalendarScreen({super.key});
+class CalendarScreen extends StatefulWidget {
+  CalendarScreen({super.key});
+
+  @override
+  State<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  DateTime focusedDay = DateTime.now();
+  DateTime? selectedDay;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Calendar Screen'),
+    return SafeArea(
+      top: true,
+      child: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: MonthHeader(),
+          ),
+          Calendar(onDaySelected: _onDaySelected(), onPageChanged: _onPageChanged(), focusedDay: focusedDay, selectedDay: selectedDay,)
+        ],
+      ),
     );
+  }
+
+  OnDaySelected _onDaySelected() {
+    return (DateTime selectedDay, DateTime focusedDay) {
+      setState(() {
+        this.selectedDay = selectedDay;
+        this.focusedDay = focusedDay;
+      });
+    };
+  }
+
+  void Function(DateTime focusedDay) _onPageChanged() {
+    return ((focusedDay) {
+      setState(() {
+        this.focusedDay = focusedDay;
+      });
+    });
   }
 }

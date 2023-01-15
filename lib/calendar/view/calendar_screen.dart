8c520a542127a4e21683aa98c286ee2ea573553c
rookedsysc <rookedsysc36@gmail.com>
2023-01-16 03:12:31 +0900

    feat : 한 달 통계 표시해주는 Header 생성(#7)

--- lib/calendar/view/calendar_screen.dart
+++ lib/calendar/view/calendar_screen.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:pocket_lab/calendar/component/month_header.dart';
 
 class CalendarScreen extends StatelessWidget {
   const CalendarScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Calendar Screen'),
+    return SafeArea(
+      top: true,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: MonthHeader(),
+          )
+        ],
+      ),
     );
   }
 }
